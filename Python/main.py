b4f0af60ba4ade828bb85360c94c5dd42cc622a4
Tim Tscheppe <timothy.tscheppe@marquette.edu>
2023-11-19 15:41:40 -0600

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,30 +1,30 @@
 import serial
 import pyautogui
-import time
 
-def scanformouseinput(ser):
+# Set the COM port to the one your Bluetooth device is connected to
+ser = serial.Serial('COM9', 9600)  # Replace 'COMx' with your COM port
+print("Connected to " + ser.portstr)
+while True:
     # Read a line of data (format: change_x,change_y,c_button,z_button)
-    data = ser.readline().decode().strip()
-    values = data.split(',')
+    try:
+        data = ser.readline().decode().strip()
+        values = data.split(',')
+    except:
+        #will get triggered if the BT connection is made while data is in the middle of being sent
+        print("Invalid data: " + data)
+        continue
+
     if len(values) != 4:
-        return 0
+        print("Invalid data: " + data)
+        continue
+    print(values)
     change_x, change_y, c_button, z_button = map(int, values)
+
     # Convert change_x and change_y to mouse movement
     pyautogui.move(change_x, change_y)
+
     # Simulate left and right clicks
     if c_button:
         pyautogui.click(button='left')
     if z_button:
         pyautogui.click(button='right')
-    return 0
-
-if __name__ == '__main__':
-    print("Scanning for bluetooth mouse, press cntrl + c to exit")
-
-    # Set the COM port to the one your Bluetooth device is connected to
-    ser = serial.Serial('COM9', 9600)  # Replace 'COMx' with your COM port
-
-    while(1):
-        scanformouseinput(ser)
-        # Wait for polling
-        time.sleep(0.01)
